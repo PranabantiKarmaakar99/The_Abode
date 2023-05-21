057b8f0bf325aa2629a560c09f196ae7ab111483
Pranabanti Karmaakar <aankkhiz@gmail.com>
2023-05-21 21:11:28 +0530

    mt spacing

--- the_abode/src/pages/components/introduction.js
+++ the_abode/src/pages/components/introduction.js
@@ -6,7 +6,7 @@ import IMG1 from "./images_home/image9.png";
 const Home = () => {
   return (
     <div id="introduction">
-      <div className="container h-screen mt-14 mb-14">
+      <div className="container h-screen mt-14 ">
         <div className=" h-1/2 bg-green-400 relative">
           <Image
             className="h-full object-cover "
